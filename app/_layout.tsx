ab8345e6b928bb47f4385dfbf5a256c54765ee50
David Hall <david.ryan.hall@gmail.com>
2024-11-02 19:33:45 -0700

    Adds react-native-gesture-handler support back. WIll be used in a cleaned up bottom shelf implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,6 +2,7 @@ import { store } from "@/state/store";
 import {Provider as ReduxProvider} from "react-redux";
 import { WebSocketProvider } from '@/context/WebSocketContext';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import AppRoot from '@/app/AppRoot';
 import { StatusBar } from 'expo-status-bar';
 import {ReactNode} from "react";
@@ -10,10 +11,12 @@ export default function Layout(): ReactNode {
   return (
     <ReduxProvider store={store}>
       <WebSocketProvider>
-        <SafeAreaProvider>
-          <StatusBar style='light' />
-          <AppRoot />
-        </SafeAreaProvider>
+        <GestureHandlerRootView>
+          <SafeAreaProvider>
+            <StatusBar style='light' />
+            <AppRoot />
+          </SafeAreaProvider>
+        </GestureHandlerRootView>
       </WebSocketProvider>
     </ReduxProvider>
   );
