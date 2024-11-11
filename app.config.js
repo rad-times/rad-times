546afa637853ac8e3d4a0fc038c77822f709aef8
David Hall <david.ryan.hall@gmail.com>
2024-11-11 12:06:40 -0800

    feat: use .env file to store oauth keys, and app.config.js to inject them into the setup files

--- app.config.js
+++ app.config.js
@@ -6,6 +6,23 @@ module.exports = ({ config }) => {
   config.android.config.googleMaps.apiKey = process.env.GOOGLE_MAPS_API_KEY;
   config.extra.eas.projectId = process.env.EAS_PROJECT_ID;
 
+  const googleAuthPluginIndex = config.plugins.findIndex(plugIn => typeof plugIn !== 'string' && plugIn[0] === '@react-native-google-signin/google-signin');
+  config.plugins[googleAuthPluginIndex][1] = {
+    "iosUrlScheme": process.env.GOOGLE_AUTH_IOS_REVERSED_ID
+  }
+
+  const facebookAuthPluginIndex = config.plugins.findIndex(plugIn => typeof plugIn !== 'string' && plugIn[0] === 'react-native-fbsdk-next');
+  config.plugins[facebookAuthPluginIndex][1] = {
+    "appID": process.env.FACEBOOK_AUTH_APP_ID,
+    "clientToken": process.env.FACEBOOK_AUTH_CLIENT_TOKEN,
+    "displayName": "Rad Times",
+    "scheme": process.env.FACEBOOK_AUTH_SCHEME,
+    "advertiserIDCollectionEnabled": false,
+    "autoLogAppEventsEnabled": false,
+    "isAutoInitEnabled": false,
+    "iosUserTrackingPermission": false
+  }
+
   if (process.env.ENVIRONMENT === 'dev') {
     config.extra.API_URL_ROOT = "http://localhost:8080";
     config.extra.WS_ROOT = "ws://localhost:8080";
